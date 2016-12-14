d8e861e8521a674cabbe12ddb44b874a2e8ab5d7
Calvin Yau <calvinj.yau@gmail.com>
2016-12-14 15:39:20 -0800

    add prototype versions of array methods

--- array.js
+++ array.js
@@ -10,6 +10,19 @@ function uniq(array) {
 
 console.log(uniq([1,3,2,1,4,3]));
 
+Array.prototype.uniq = function () {
+  let uniqs = [];
+  for (let i = 0; i < this.length; i++) {
+    if (uniqs.indexOf(this[i]) === -1) {
+      uniqs.push(this[i]);
+    }
+  }
+  return uniqs;
+};
+
+console.log([1,3,2,1,4,3].uniq());
+
+
 function twoSum(array) {
   let pairs = [];
   for (let i = 0; i < array.length; i++) {
@@ -23,6 +36,21 @@ function twoSum(array) {
 }
 
 console.log(twoSum([-1, 0, 2, -2, 1]));
+
+Array.prototype.twoSum = function () {
+  let pairs = [];
+  for (let i = 0; i < this.length; i++) {
+    for (let j = i + 1; j < this.length; j++) {
+      if (this[i] + this[j] === 0) {
+        pairs.push([i, j]);
+      }
+    }
+  }
+  return pairs;
+};
+
+console.log([-1, 0, 2, -2, 1].twoSum());
+
 
 function transpose(array) {
   let transposed = [];
@@ -39,3 +67,20 @@ function transpose(array) {
 console.log(transpose([[0, 1, 2],
                       [3, 4, 5],
                       [6, 7, 8]]));
+
+Array.prototype.transpose = function () {
+  let transposed = [];
+  for (let i = 0; i < this[0].length; i++) {
+    let row = [];
+    for (let rowIdx = 0; rowIdx < this.length; rowIdx++) {
+      row.push(this[rowIdx][i]);
+    }
+    transposed.push(row);
+  }
+  return transposed;
+};
+
+
+console.log([[0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8]].transpose());
